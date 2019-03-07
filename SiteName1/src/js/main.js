fec3d1ebbf29f11b20c5f6f9cf523d78240b72b8
olehsova-hyuna <oleh.sova@hyuna.bb>
2019-03-07 18:58:37 +0200

    add content

--- SiteName1/src/js/main.js
+++ SiteName1/src/js/main.js
@@ -1,4 +1,20 @@
 //= ../../bower_components/jquery/dist/jquery.js
+//= partials/owl.carousel.min.js
+
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+		center: true,
+		items: 3,
+		loop:true,
+		navigation: true,
+		margin: 30,
+		responsive:{
+			600:{
+				items:4
+			}
+		}
+	});
+});
 
 
 
