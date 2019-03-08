1ff6f84bcf83bd8eae5c488c0e357f31358f5631
olehsova-hyuna <oleh.sova@hyuna.bb>
2019-03-08 19:03:08 +0200

    commit

--- SiteName1/src/js/main.js
+++ SiteName1/src/js/main.js
@@ -3,14 +3,39 @@
 
 $(document).ready(function(){
 	$('.owl-carousel').owlCarousel({
-		center: true,
-		items: 3,
-		loop:true,
-		navigation: true,
-		margin: 30,
+		items: 5,
+		dots: true,
+		lazyLoad: true,
 		responsive:{
-			600:{
-				items:4
+			0:{
+				items:1,
+				loop:false,
+				center:true,
+				loop:true,
+				autoWidth:true
+			},
+			784:{
+				items:2,
+				loop:false,
+				center:true,
+				loop:true,
+				autoWidth:true
+			},
+			1190:{
+				items:3,
+				margin:30,
+				loop:false,
+				center:true,
+				loop:true,
+				autoWidth:true				
+			},
+			1527:{
+				items:4,
+				margin:30,
+				loop:false,
+				center:true,
+				loop:true,
+				autoWidth:false		
 			}
 		}
 	});
